5fe9930431e7431638311f6b4681784227972fa0
Marcolvv <marcoclaudio102@hotmail.com>
2024-04-11 04:02:47 -0600

    se agrego plugin para el manejo de imagenes

--- Coppel-page/coppel/vite.config.js
+++ Coppel-page/coppel/vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import reactSvgPlugin from 'vite-plugin-react-svg'
+import { ViteImagesPlugin } from 'vite-plugin-images'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    reactSvgPlugin(),
+    ViteImagesPlugin()],
 })
